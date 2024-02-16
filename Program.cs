93945c1cc8becfd3a685896244f84215aa90c8bf
hcobbo <hcobbo@gmail.com>
2024-02-16 14:38:20 +0900

    добавил систему координат, изменил класс программ, изменил класс Order, добавил класс Point, добавил евклидову формулу(добавил метод DistanceTo), поменял название переменных

--- Program.cs
+++ Program.cs
@@ -1,35 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+
+
+public class CoordinateSystem
+{
+    public int Width {get;}
+    public int Height{get;}
+    public CoordinateSystem(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
 class Order
 {
-    
+        public Point PointA {get; set;} //точка где забирают посылку
+        public Point PointB {get; set;} //точка куда надо доставить посылку
         public int List {get;set;}
         public int RandomPrice {get;set;}
         public List<int>[] Distance {get; set;}
     
 }
+class Point
+{
+    public int X {get; set;}
+    public int Y {get; set;}
+
+    public Point (int x, int y)
+    {
+        X=x;
+        Y=y;
+
+    }
+     public double DistanceTo(Point other)
+    {
+        return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2)); //евлидово расстояние(формула)
+    }
+}
 
 class Courier
 {
-    public int RandomDistance {get; set;}
+    public int Distance {get; set;}
 }
+
 class Program
 {
    
         static void Main(string[] args)
         {
+        Random random = new Random();
 
-        Random Random = new Random();
+        Point pointA = new Point(random.Next(1, 1000), random.Next(1, 1000)); 
+        Point pointB = new Point(random.Next(1, 1000), random.Next(1, 1000));
+        Point courierLocation = new Point(random.Next(1, 1000), random.Next(1, 1000));
+
+
+        Console.WriteLine($"Координаты точки А: ({pointA.X}, {pointA.Y})");
+        Console.WriteLine($"Координаты точки Б: ({pointB.X}, {pointB.Y})");
+        Console.WriteLine($"местоположение курьера: ({courierLocation.X}, {courierLocation.Y})");
+
+        Random RandomNumber = new Random();
 
         Order obj = new Order();
         obj.List = 10;
-        obj. RandomPrice = Random.Next(100,500);
+        obj. RandomPrice = RandomNumber.Next(100,500);
 
         Courier courier = new Courier();
-        courier.RandomDistance = Random.Next(100,3000);
+        courier.Distance = (int)courierLocation.DistanceTo(pointA);
 
         Console.WriteLine($"Цена заказа: {obj.RandomPrice}");
-        Console.WriteLine($"Расстояние до заказа: {courier.RandomDistance} метров");
+        Console.WriteLine($"Расстояние до заказа: {courier.Distance} метров"); // 
         }
 }
 
--- obj/Debug/net8.0/DriveeTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/DriveeTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveeTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543165578b771b5041a4c3c3429b4d192d02629c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38c85a5f54934715aa189802b1aa74f60d38f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveeTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveeTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
