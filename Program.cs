a38c85a5f54934715aa189802b1aa74f60d38f27
hcobbo <hcobbo@gmail.com>
2024-02-16 11:27:34 +0900

    создание классов, рандомайзера для теста

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+class Order
+{
+    
+        public int List {get;set;}
+        public int RandomPrice {get;set;}
+        public List<int>[] Distance {get; set;}
+    
+}
+
+class Courier
+{
+    public int RandomDistance {get; set;}
+}
+class Program
+{
+   
+        static void Main(string[] args)
+        {
+
+        Random Random = new Random();
+
+        Order obj = new Order();
+        obj.List = 10;
+        obj. RandomPrice = Random.Next(100,500);
+
+        Courier courier = new Courier();
+        courier.RandomDistance = Random.Next(100,3000);
+
+        Console.WriteLine($"Цена заказа: {obj.RandomPrice}");
+        Console.WriteLine($"Расстояние до заказа: {courier.RandomDistance} метров");
+        }
+}
+
